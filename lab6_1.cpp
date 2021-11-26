8c3c18725add6032db4ff1ad5d56683d4c7862ef
gusntc <Natacha_rungbanpant@cmu.ac.th>
2021-11-26 16:59:59 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -4,15 +4,17 @@ using namespace std;
 int main(){
     int num;
     int even=0;
-    int odd=-1;
+    int odd=0;
+    cout << "Enter an integer: ";
+    cin >> num;
     while(num != 0){
         if(num%2 == 0){
         even++;
         }else{
         odd++;
         }
-    cout << "Enter an integer: ";
-    cin >> num;
+        cout << "Enter an integer: ";
+        cin >> num;
     }
     cout << "#Even numbers = " << even << "\n";
     cout << "#Odd numbers = " << odd;
